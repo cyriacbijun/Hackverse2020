a59ab9f3d7aba8cc7a0dfd1505e77e130599c491
CyriacBijuN <cyriacbijun@gmail.com>
2020-02-26 18:01:54 +0530

    hidden API keys

--- src/App.js
+++ src/App.js
@@ -3,14 +3,7 @@ import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
 import * as firebase from 'firebase'
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBke7BqlyFa5PA8mIjG23JfHFmt-R0_jG8",
-    authDomain: "hackverse-1579951297563.firebaseapp.com",
-    databaseURL: "https://hackverse-1579951297563.firebaseio.com",
-    projectId: "hackverse-1579951297563",
-    storageBucket: "hackverse-1579951297563.appspot.com",
-    messagingSenderId: "98768578088",
-    appId: "1:98768578088:web:489508f4709fe575efb71d",
-    measurementId: "G-PJ2JH3BBEP"
+    ....
   };
 firebase.initializeApp(firebaseConfig);
 const mapStyles = {
